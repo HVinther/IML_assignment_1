42780345841d87dc9cf9c830444b58d52d369e02
Hans Vinther-Larsen <hans@vinther-larsen.dk>
2024-02-26 14:24:30 +0100

    Tilføjede kommentarer til funktionen loadData.

--- utils.R
+++ utils.R
@@ -2,6 +2,7 @@ library(CASdatasets)
 library(splitTools)
 
 loadData<-function(seed = 2024){
+  # Henter datasættene som beskrevet, men i funktionens enviroment istedet for det globale
   data(freMPL1,envir = environment())
   
   data(freMPL2,envir = environment())
@@ -18,8 +19,13 @@ loadData<-function(seed = 2024){
   
   set.seed(seed) 
   
+  # De relevante objekter skubbes ud til det globale enviroment
+  freMPL <<- freMPL
   ind <<- partition(freMPL$ClaimInd, p = c(train = 0.8, test = 0.2)) #### train and test have the same claim frequency
   train <<- freMPL[ind$train, ] 
   test <<- freMPL[ind$test, ]
+  
+  # Tømmer alt fra det funktionens enviroment, ie. de store dataset, som allerede er gemt i det globale enviroment
+  rm(list = ls())
 }
 
