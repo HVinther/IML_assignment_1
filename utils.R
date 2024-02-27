9606637892dc12b94ec6de4d4f5ebe337930373e
Christopher Fjeldberg Jensen <chridifj@gmail.com>
2024-02-27 15:14:08 +0100

    Jeg ændrede også seeden

--- utils.R
+++ utils.R
@@ -1,4 +1,4 @@
-loadData<-function(seed = 2024){
+loadData<-function(seed = 2022){
   # Checker dependencies og evt. installere disse
   dependencies <- c("xts","sp","zoo","CASdatasets","splitTools")
   md<-c()
