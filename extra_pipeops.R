1d9d170c19711659e023f8450dde2a926b16f0c8
Hans Vinther-Larsen <hans@vinther-larsen.dk>
2024-02-29 11:13:16 +0100

    po har nu fået id'er og overskriver de originale variable, hvor det er forventet.

--- extra_pipeops.R
+++ extra_pipeops.R
@@ -6,47 +6,47 @@
 po_RecEnd_rc<-po(
   "mutate",
   mutation = list(
-    RecordEnd_num = as.numeric(ifelse(is.na(RecordEnd),
-                                      max(RecordBeg),
+    RecordEnd = ~as.numeric(ifelse(is.na(RecordEnd),
+                                      max(na.omit(RecordEnd)),
                                       RecordEnd)),
-    RecordEnd_censored = is.na(RecordEnd)
+    RecordEnd_censored = ~is.na(RecordEnd)
   ),
-  delete_originals = TRUE
+  id = "RecEnd_rc"
 )
 
 # Reencodes RecordBeg (POSIXct) to numeric
 po_RecBeg_num<-po(
   "mutate",
   mutation = list(
-    RecordBeg_num = as.numeric(RecordBeg)
+    RecordBeg = ~as.numeric(RecordBeg)
   ),
-  delete_originals = TRUE
+  id = "RecBeg_num"
 )
 
 # Reencodes SocioCateg as an integer using the numbering provided in the factor
 po_SocCat_int<-po(
   "mutate",
+  id = "SocioCateg_as_integer",
   mutation = list(
-    SocioCateg_int = as.integer(substr(SocioCateg,4,5))),
-  delete_originals = TRUE
+    SocioCateg = ~as.integer(substr(SocioCateg,4,5))),
 )
 
 # Reencodes VehPrice as integer using the default ordering
 po_VehPrice_int<-po(
   "mutate",
+  id = "VehPrice_as_integer",
   mutation = list(
-    SocioCateg_int = as.integer(VehPrice)),
-  delete_originals = TRUE
+    VehPrice = ~as.integer(VehPrice))
 )
 
 # Reencodes VehAge as numeric using the "mean" of provided intervals
 po_VehAge_num<-po(
   "mutate",
+  id = "VehAge_as_num",
   mutation = list(
-    VehAge_num = case_match(VehAge,
+    VehAge = ~case_match(VehAge,
                             "6-7" ~ 6.5,
                             "8-9" ~ 8.5,
                             "10+" ~ 10,
                             .default = as.numeric(VehAge))),
-  delete_originals = TRUE
 )
