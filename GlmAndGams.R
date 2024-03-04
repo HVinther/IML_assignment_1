879f8b21908e610267981fa6dedf3d62315aa8b5
Hans Vinther-Larsen <hans@vinther-larsen.dk>
2024-03-04 15:47:25 +0100

    Udkast til glmnet. Der er en simpel xgboost og featureless med til sammenligning, men det er ikke så vigtigt.

---
+++ GlmAndGams.R
@@ -0,0 +1,76 @@
+source("libraries.R")
+source("utils.R")
+source("extra_pipeops.R")
+loadData()
+
+train_task <- as_task_regr(train,target = "ClaimAmount")
+
+grph_full<-
+  po_SocCat_int%>>%
+  po_VehAge_num%>>%
+  po_VehPrice_int%>>%
+  po_RecBeg_num %>>%
+  po_RecEnd_rc %>>%
+  po("encode")%>>%
+  po("scale")
+
+grph_base<-
+  po_RecBeg_num %>>%
+  po_RecEnd_rc %>>%
+  po("encode")%>>%
+  po("scale")
+
+lrn_glmnet<-
+  lrn("regr.cv_glmnet") |>
+  set_id(id = "cv_glmnet")
+
+lrn_xboost<-
+  lrn("regr.xgboost") |>
+  set_id("xgboost")
+
+graph_list<-list(
+  "full" = grph_full,
+  "base" = grph_base
+)
+
+learner_list<-list(
+  lrn_glmnet,
+  lrn_xboost
+)
+
+
+future::plan("multisession")
+
+bm<-benchmark(
+  benchmark_grid(
+    tasks = list(
+      train_task,
+      add_weight(train_task,"interest"),
+      add_weight(train_task,"frequency"),
+      add_weight(train_task)
+    ),
+    learners = append(
+      combine_graphs_and_learners(
+        graph_list,
+        learner_list),
+      lrn("regr.featureless")),
+    resamplings = rsmp("cv",folds = 5)
+  )
+)
+
+future::plan("sequential")
+
+bm$aggregate()
+
+bm$aggregate(list(msr("regr.mse"),msr("time_train")))|>
+  ggplot()+
+  geom_hline(aes(yintercept = log(regr.mse), 
+                 color = learner_id),
+             linetype = "dashed",
+             linewidth = 1,
+             alpha = 0.8)+
+  geom_point(aes(x= time_train,
+                 y = log(regr.mse),
+                 color = learner_id,
+                 shape = task_id),
+             size = 5)
