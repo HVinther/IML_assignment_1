23a8c34de81f949c73e82ee913d91b7f763f3564
Hans Vinther-Larsen <hans@vinther-larsen.dk>
2024-03-06 16:29:51 +0100

    start på learner def

---
+++ comparison_of_learners_on_train.R
@@ -0,0 +1,42 @@
+source("libraries.R")
+source("utils.R")
+source("extra_pipeops.R")
+source("grph_lists.R")
+loadData()
+
+train_task <- as_task_regr(train,target = "ClaimAmount")
+
+
+## gam and ensemble learners
+lrn_gam<-
+  lrn("regr.gam")|>
+  set_id("gam")
+
+lrn_ensemble<-
+  gunion(list(
+    po("learner_cv",
+       learner = lrn("regr.gam"),
+       id = "gam",
+       resampling.folds = 2),
+    po("learner_cv",
+       learner = lrn("regr.xgboost"),
+       id = "xgboost",
+       resampling.folds = 2))
+  ) %>>%
+  po("featureunion") %>>%
+  po("learner", learner = lrn("regr.lm")) |>
+  as_learner()|>
+  set_id("ensemble")
+
+lrn_list_ge<-list(
+  lrn_gam,
+  lrn_ensemble)
+
+combine_graphs_and_learners(
+  list("num.impact"= 
+         po_RecBeg_num %>>%
+         po_RecEnd_rc %>>%
+         po("scale")%>>%
+         po("encodeimpact")),
+  lrn_list_ge
+  )
